46a18f8e1e1408fcdaff615d5f8810ab5430283d
Tetsuya M Bassler <basslert@oregonstate.edu>
2026-05-31 12:36:30 -0700

    Update task.py
    add conv_endian with big and little endian support

--- task.py
+++ task.py
@@ -5,4 +5,35 @@ def my_datetime(num_sec):
     pass
 
 def conv_endian(num, endian='big'):
-    pass
+    if endian != 'big' and endian != 'little':
+        return None
+        
+    is_negative = num < 0
+    num = abs(num)
+    
+    if num == 0:
+        return '00'
+        
+    # Convert number to hex string
+    hex_chars = '0123456789ABCDEF'
+    hex_str = ''
+    while num > 0:
+        hex_str = hex_chars[num % 16] + hex_str
+        num = num // 16
+        
+    # Padding to even
+    if len(hex_str) % 2 != 0:
+        hex_str = '0' + hex_str
+        
+    # Split into bytes
+    bytes_list = [hex_str[i:i+2] for i in range(0, len(hex_str), 2)]
+    
+    if endian == 'little':
+        bytes_list.reverse()
+        
+    result = ' '.join(bytes_list)
+    
+    if is_negative:
+        result = '-' + result
+        
+    return result
